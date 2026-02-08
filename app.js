765bf20e1537761e97502ac91f84024b9e4212d3
gaarpoi098 <kravcove893@gmail.com>
2026-02-08 13:09:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-const canvas = document.getElementById("snow");
+const canvas = document.createElement("canvas");
+canvas.id = "snow";
+document.body.appendChild(canvas);
+
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
 
-const flakes = Array.from({length: 120}).map(() => ({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    r: Math.random() * 3,
-    s: Math.random() * 2 + 1
+const flakes = Array.from({length: 150}).map(() => ({
+    x: Math.random()*canvas.width,
+    y: Math.random()*canvas.height,
+    r: Math.random()*3+1,
+    s: Math.random()*2+1
 }));
 
-function draw() {
+function snow() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle = "white";
 
-    flakes.forEach(f => {
+    flakes.forEach(f=>{
         ctx.beginPath();
-        ctx.arc(f.x, f.y, f.r, 0, Math.PI*2);
+        ctx.arc(f.x,f.y,f.r,0,Math.PI*2);
         ctx.fill();
 
-        f.y += f.s;
-        if(f.y > canvas.height) f.y = 0;
+        f.y+=f.s;
+        if(f.y>canvas.height) f.y=0;
     });
 
-    requestAnimationFrame(draw);
+    requestAnimationFrame(snow);
 }
 
-draw();
+snow();
